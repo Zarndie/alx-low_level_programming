9b5b302d9ac4d57c70ac83eb44672b7062e9ce47
zarndie <zandilegwen@gmail.com>
2023-03-28 05:29:25 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,11 +4,12 @@
 /**
  * print_ray - prints n elements of an array of integers
  * @a: array of integers
- * @n: number of elements of the arrays to be printed
+ * @n: number of elements of the array to be printed
  * return: void
  */
 
 void print_array(int *a, int n)
+
 {
 
 	int j;
