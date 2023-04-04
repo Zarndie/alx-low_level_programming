5fbdd82294429d7b60c8a85852beb13e56f255cc
zarndie <zandilegwen@gmail.com>
2023-04-04 14:38:11 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: Always 0 (success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+	int i, n;
+
+	for (i = 0; i < 0; i++)
+	{
+
+		for (n = 0; n < 0; n++)
+			putchar(a[i][n]);
+		putchar('\n');
+
+	}
+
+}
