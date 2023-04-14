db9a7a6901cb6c044325c95c2a8785f5552f4e48
zarndie <zandilegwen@gmail.com>
2023-04-13 18:43:30 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
 	unsigned int i = 0, j = 0, k = 0, l = 0;
-     	char *str;
+	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -27,28 +27,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		k++;
 
 	if (n >= k)
-                l = i + k;
-        else
-                l = i + n;
+		l = i + k;
+	else
+		l = i + n;
 
-        str = malloc(sizeof(char) * l + 1);
-        if (str == NULL)
-                return (NULL);
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
 
-        k = 0;
-        while (j < l)
-        {
-                if (j <= i)
-                        str[j] = s1[j];
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
 
-                if (j >= i)
-                {
-                        str[j] = s2[k];
-                        k++;
-                }
-                j++;
-        }
-        str[j] = '\0';
-        return (str);
-
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
